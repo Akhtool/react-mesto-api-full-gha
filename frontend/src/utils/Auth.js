3aaff659179f3d7a07330b7c56ee8757bc062096
Akhtool <Zelimhan1997@mail.ru>
2023-07-12 20:11:22 +0300

    fix: fix url

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -47,7 +47,7 @@ class Auth {
 const auth = new Auth({
   // baseUrl: "https://auth.nomoreparties.co",
   // baseUrl: "http://localhost:3000",
-  baseUrl: "https://api.akhtool.mesto.nomoredomains.work",
+  baseUrl: "http://api.akhtool.mesto.nomoredomains.work",
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,7 @@
 export const apiOptions = {
   // baseUrl: "https://mesto.nomoreparties.co/v1/cohort-62",
   // baseUrl: "http://localhost:3000",
-  baseUrl: "https://api.akhtool.mesto.nomoredomains.work",
+  baseUrl: "http://api.akhtool.mesto.nomoredomains.work",
   headers: {
     // authorization: "6140e03a-cdf5-4314-8120-d3eda6855951",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
